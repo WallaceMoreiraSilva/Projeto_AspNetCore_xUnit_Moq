464233e25e38bbb64eba830ebad8ae2a7c3ef523
Wallace M <wallaceinfofuturo@gmail.com>
2022-05-21 17:43:56 -0300

    adicionado comentarios para entendimento do mock

--- Projeto_AspNetCore_xUnit_Moq/Projeto_AspNetCore_xUnit_Moq.Testes/CadastraTarefaHandlerExecute.cs
+++ Projeto_AspNetCore_xUnit_Moq/Projeto_AspNetCore_xUnit_Moq.Testes/CadastraTarefaHandlerExecute.cs
@@ -40,11 +40,16 @@ namespace Projeto_AspNetCore_xUnit_Moq.Testes
             //arrange
             var comando = new CadastraTarefa("Estudar Xunit", new Categoria("Estudo"), new DateTime(2019, 12, 31));
 
+            //crio uma variavel que vai receber uma variavel do tipo mock e preciso informar para qual tipo ela vai ser
             var mock = new Mock<IRepositorioTarefas>();
 
+            //Setup é o seguinte: quando o IncluirTarefas for chamado vai lançar uma excecao. 
+            //o metodo IncluirTarefas tem como argumento um array de tarefas entao precisa dizer para o mock que essa excecao
+            //vai ser lancada pra qualquer array de tarefas que for chamado
             mock.Setup(r => r.IncluirTarefas(It.IsAny<Tarefa[]>()))
                 .Throws(new Exception("Houve um erro na inclusão de tarefas"));
 
+            //objeto que esta sobre sua responsabilidade do mock
             var repo = mock.Object;
 
             var handler = new CadastraTarefaHandler(repo);
