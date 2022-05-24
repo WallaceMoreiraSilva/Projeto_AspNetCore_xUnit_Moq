b5d27c7f7e702996dd4e8753ac366034b4bd18ae
Wallace M <wallaceinfofuturo@gmail.com>
2022-05-24 17:09:45 -0300

    Adicionando teste de log

--- Projeto_AspNetCore_xUnit_Moq/Projeto_AspNetCore_xUnit_Moq.Testes/CadastraTarefaHandlerExecute.cs
+++ Projeto_AspNetCore_xUnit_Moq/Projeto_AspNetCore_xUnit_Moq.Testes/CadastraTarefaHandlerExecute.cs
@@ -1,12 +1,13 @@
-using Microsoft.EntityFrameworkCore;
-using Moq;
 using Projeto_AspNetCore_xUnit_Moq.Core.Commands;
 using Projeto_AspNetCore_xUnit_Moq.Core.Models;
 using Projeto_AspNetCore_xUnit_Moq.Infrastructure;
 using Projeto_AspNetCore_xUnit_Moq.Services.Handlers;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Linq;
 using Xunit;
+using Moq;
+using Microsoft.Extensions.Logging;
 
 namespace Projeto_AspNetCore_xUnit_Moq.Testes
 {
@@ -16,7 +17,9 @@ namespace Projeto_AspNetCore_xUnit_Moq.Testes
         public void DadaTarefaComInfoValidasDeveIncluirNoBD()
         {
             //arrange
-            var comando = new CadastraTarefa("Estudar Xunit", new Categoria("Estudo"), new DateTime(2019, 12, 31));
+            var comando = new CadastraTarefa("Estudar Xunit", new Categoria(100, "Estudo"), new DateTime(2019, 12, 31));
+
+            var mock = new Mock<ILogger<CadastraTarefaHandler>>();
 
             var options = new DbContextOptionsBuilder<DbTarefasContext>()
                 .UseInMemoryDatabase("DbTarefasContext")
@@ -24,7 +27,7 @@ namespace Projeto_AspNetCore_xUnit_Moq.Testes
             var contexto = new DbTarefasContext(options);
             var repo = new RepositorioTarefa(contexto);
 
-            var handler = new CadastraTarefaHandler(repo);
+            var handler = new CadastraTarefaHandler(repo, mock.Object);
 
             //act
             handler.Execute(comando); //SUT >> CadastraTarefaHandlerExecute
@@ -34,25 +37,73 @@ namespace Projeto_AspNetCore_xUnit_Moq.Testes
             Assert.NotNull(tarefa);
         }
 
+        //LogLevel é o nivel de log que estou querendo. No nosso caso é o log de erro
+        //EventId é o identificador do acontecimento.
+        //state é a mensagem que a gente quer, o objeto que esta sendo logado. No nosso caso é "Houve um erro na inclusão de tarefas"
+        //exception é a exceção lançada
+        //Func<object, Exception, string> é uma funcao que pega o objeto que foi logado e a excecao e transforma isso em uma strig
+        //criei um delegate que vai ser usado para capturar quando esse metodo for passado
+        delegate void CapturaMensagemLog(LogLevel level, EventId eventId, object state, Exception exception, Func<object, Exception, string> function);
+
+        [Fact]
+        public void DadaTarefaComInfoValidasDeveLogar()
+        {
+            //arrange
+            var tituloTarefaEsperado = "Usar Moq para aprofundar conhecimento de API";
+            var comando = new CadastraTarefa(tituloTarefaEsperado, new Categoria(100, "Estudo"), new DateTime(2019, 12, 31));
+
+            var mockLogger = new Mock<ILogger<CadastraTarefaHandler>>();
+
+            LogLevel levelCapturado = LogLevel.Error;
+            string mensagemCapturada = string.Empty;
+
+            //aqui criamos uma instancia do tipo delegate
+            //Aqui temos a funcao que vai ser chamada pelo Callback
+            CapturaMensagemLog captura = (level, eventId, state, exception, func) =>
+            {
+                levelCapturado = level;
+                mensagemCapturada = func(state, exception);
+            };
+
+            //faça uma configuracao para o metodo ILog pra qualquer argumento de entrada
+            mockLogger.Setup(l =>
+                l.Log(
+                    It.IsAny<LogLevel>(), //nível de log => LogError
+                    It.IsAny<EventId>(), //identificador do evento
+                    It.IsAny<object>(), //objeto que será logado
+                    It.IsAny<Exception>(),    //exceção que será logada
+                    It.IsAny<Func<object, Exception, string>>() //função que converte objeto+exceção >> string)
+                )).Callback(captura); //vou chamar uma funcao quando esse metodo ILog for chamado
+
+            //vou criar um mock para simular o repositorio de tarefas
+            var mock = new Mock<IRepositorioTarefas>();
+           
+            var handler = new CadastraTarefaHandler(mock.Object, mockLogger.Object);
+
+            //act
+            handler.Execute(comando); //SUT >> CadastraTarefaHandlerExecute
+
+            //assert
+            Assert.Equal(LogLevel.Debug, levelCapturado);
+            Assert.Contains(tituloTarefaEsperado, mensagemCapturada);
+        }
+
         [Fact]
         public void QuandoExceptionForLancadaResultadoIsSuccessDeveSerFalse()
         {
             //arrange
             var comando = new CadastraTarefa("Estudar Xunit", new Categoria("Estudo"), new DateTime(2019, 12, 31));
 
-            //crio uma variavel que vai receber uma variavel do tipo mock e preciso informar para qual tipo ela vai ser
+            var mockLogger = new Mock<ILogger<CadastraTarefaHandler>>();
+
             var mock = new Mock<IRepositorioTarefas>();
 
-            //Setup é o seguinte: quando o IncluirTarefas for chamado vai lançar uma excecao. 
-            //o metodo IncluirTarefas tem como argumento um array de tarefas entao precisa dizer para o mock que essa excecao
-            //vai ser lancada pra qualquer array de tarefas que for chamado
             mock.Setup(r => r.IncluirTarefas(It.IsAny<Tarefa[]>()))
                 .Throws(new Exception("Houve um erro na inclusão de tarefas"));
 
-            //objeto que esta sobre sua responsabilidade do mock
             var repo = mock.Object;
 
-            var handler = new CadastraTarefaHandler(repo);
+            var handler = new CadastraTarefaHandler(repo, mockLogger.Object);
 
             //act
             CommandResult resultado = handler.Execute(comando);
@@ -60,5 +111,41 @@ namespace Projeto_AspNetCore_xUnit_Moq.Testes
             //assert
             Assert.False(resultado.IsSuccess);
         }
+
+        [Fact]
+        public void QuandoExceptionForLancadaDeveLogarAMensagemDaExcecao()
+        {
+            //arrange
+            var mensagemDeErroEsperada = "Houve um erro na inclusão de tarefas";
+            var excecaoEsperada = new Exception(mensagemDeErroEsperada);
+
+            var comando = new CadastraTarefa("Estudar Xunit", new Categoria("Estudo"), new DateTime(2019, 12, 31));
+
+            var mockLogger = new Mock<ILogger<CadastraTarefaHandler>>();
+
+            var mock = new Mock<IRepositorioTarefas>();
+
+            mock.Setup(r => r.IncluirTarefas(It.IsAny<Tarefa[]>()))
+                .Throws(excecaoEsperada);
+
+            var repo = mock.Object;
+
+            var handler = new CadastraTarefaHandler(repo, mockLogger.Object);
+
+            //act
+            CommandResult resultado = handler.Execute(comando);
+
+            //assert
+            mockLogger.Verify(l =>
+                l.Log(
+                    LogLevel.Error, //nível de log => LogError
+                    It.IsAny<EventId>(), //identificador do evento
+                    It.IsAny<object>(), //objeto que será logado
+                    excecaoEsperada,    //exceção que será logada
+                    It.IsAny<Func<object, Exception, string>>()
+                ), //função que converte objeto+exceção >> string
+                Times.Once());
+        }
+
     }
 }
